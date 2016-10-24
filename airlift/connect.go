4fc04953154ac638b4c3a2f944092c167fe91e39
Jason Chu <jason@Jasons-MacBook-Pro.local>
2016-10-25 00:07:11 +0800

    Add configurable profiling

--- airlift/connect.go
+++ airlift/connect.go
@@ -1,7 +1,10 @@
 package airlift
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"time"
 
 	r "github.com/dancannon/gorethink"
 )
@@ -9,6 +12,8 @@ import (
 // ErrNotFound is the error that is returned if the document being queried
 // could not be found.
 var ErrNotFound = errors.New("airlift: not found")
+
+const enableProfiling = false
 
 var session *r.Session
 
@@ -18,4 +23,44 @@ func Connect(opts r.ConnectOpts) error {
 	session, err = r.Connect(opts)
 
 	return err
+}
+
+func getOne(term r.Term, result interface{}) error {
+	start := time.Now()
+	c, err := term.Run(session, r.RunOpts{
+		Profile: enableProfiling,
+	})
+	if err != nil {
+		return err
+	}
+
+	if enableProfiling {
+		if time.Since(start) > time.Millisecond*90 {
+			fmt.Println("Slow query warning, took", time.Since(start).Seconds(), "seconds")
+			resp, _ := json.MarshalIndent(c.Profile(), "", "  ")
+			fmt.Println(string(resp))
+		}
+	}
+
+	return c.One(result)
+}
+
+func getAll(term r.Term, result interface{}) error {
+	start := time.Now()
+	c, err := term.Run(session, r.RunOpts{
+		Profile: enableProfiling,
+	})
+	if err != nil {
+		return err
+	}
+
+	if enableProfiling {
+		if time.Since(start) > time.Millisecond*90 {
+			fmt.Println("Slow query warning, took", time.Since(start).Seconds(), "seconds")
+			resp, _ := json.MarshalIndent(c.Profile(), "", "  ")
+			fmt.Println(string(resp))
+		}
+	}
+
+	return c.All(result)
 }
